8b0af4dfe51e07373f3cc64e84a005a207d5a821
Sashank Aryal <sashankaryal@bennington.edu>
2018-05-01 23:55:25 -0400

    Setup redux env for managing state

--- frontend/README.md
+++ frontend/README.md
@@ -1,3 +1,11 @@
+# Frontend
+
+## To-do
+
+- [ ] Use redux for state management
+- [ ] CI/CD pipeline using AWS
+- [ ] Write unit tests for the frontend using Jest-Enzyme
+
 ## Build locally in dev mode ##
 `npm update`
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Store from './redux/store';
 
 import './App.css';
 import Routes from './Routes';
@@ -8,9 +10,11 @@ import Routes from './Routes';
 export default class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Routes />
-      </Router>
+      <Provider store={Store}>
+        <Router>
+          <Routes />
+        </Router>
+      </Provider>
     );
   }
 }
